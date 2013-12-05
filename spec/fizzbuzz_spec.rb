0ea46efa44ca1fe5699bf35018ea1d3947d27e8a
cierra burns <cburns@chicagotechacademy.org>
2013-12-05 14:56:37 -0600

    solve first test

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec' 
+require 'fizzbuzz.rb' 
+
+describe 'Caculate claas' do 
+     it 'should return 1 for 1' do   
+      @caculate = Caculator.new
+    @Caculate.Caculator(1).should == 1
+   end  
+end 
